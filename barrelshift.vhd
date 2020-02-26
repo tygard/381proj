cfb8c96937cfcffb26c8c6cc1a2c169bff71922b
zbesta <zbesta@iastate.edu>
2020-02-25 18:43:10 -0600

    Added first level of barrel shifter

--- barrelshift.vhd
+++ barrelshift.vhd
@@ -13,9 +13,9 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity barrelshift is
-  generic(N : integer := 4); -- Generic of type integer for input/output data width. Default value is 32.
+  generic(N : integer := 32); -- Generic of type integer for input/output data width. Default value is 32.
   
-  port(i_SHAMT      : in std_logic_vector(1 downto 0)		 -- Shift amount input
+  port(i_SHAMT      : in std_logic_vector(4 downto 0);		 -- Shift amount input
        i_D	        : in std_logic;     					 -- Shift direction input (0: left, 1: right)
        i_T          : in std_logic;     					 -- Shift type input (0: logical, 1: arith.)
 	   i_X          : in std_logic_vector(N-1 downto 0);     -- Data value input
@@ -25,14 +25,31 @@ end barrelshift;
 
 architecture structural of barrelshift is
 
-	component mux2t1
-		port(i_S                  : in std_logic;
-			 i_D0                 : in std_logic;
-			 i_D1                 : in std_logic;
-			 o_O                  : out std_logic);
+	-- Signals to handle connections between the 5 MUX rows A-H
+	signal s_iA : std_logic_vector(N downto 0);
+	signal s_iB : std_logic_vector(N downto 0);
+	signal s_iC : std_logic_vector(N downto 0);
+	signal s_iD : std_logic_vector(N downto 0);
+	signal s_iE : std_logic_vector(N downto 0);
+	
+	
+	-- N-bit 2:1 MUX
+	component mux2t1_N
+		port(i_S          : in std_logic;
+			 i_D0         : in std_logic_vector(N-1 downto 0);
+			 i_D1         : in std_logic_vector(N-1 downto 0);
+			 o_O          : out std_logic_vector(N-1 downto 0));
 	end component;
+	
 	begin
-
-		-- Todo: define connections
+		---------------------------------------------------------------------------
+		-- Level 0: leftmost MUX array configured to shift 1 bit left
+		---------------------------------------------------------------------------
+		g_MUXA: mux2t1_N
+		port MAP(i_S               => i_SHAMT(4),
+				 i_D0			   => i_X,
+				 i_D1              => "0" & i_X(30 downto 0),
+				 o_O               => s_iA);
+		
   
 end structural;
