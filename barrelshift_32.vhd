a100f3a9eb006a11d3fb6cfae09204a2e2db2e4e
zbesta <zbesta@iastate.edu>
2020-03-02 20:13:15 -0600

    Updating barrel shifter testbench

--- barrelshift_32.vhd
+++ barrelshift_32.vhd
@@ -13,7 +13,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity barrelshift_32 is
-  
+  generic(N : integer := 32); -- Generic of type integer for input/output data width. Default value is 32.
   port(i_SHAMT      : in std_logic_vector(4 downto 0);		 -- Shift amount input
        i_D	        : in std_logic;     					 -- Shift direction input (0: left, 1: right)
        i_T          : in std_logic;     					 -- Shift type input (0: logical, 1: arith.)
@@ -29,14 +29,15 @@ architecture structural of barrelshift_32 is
 	signal s_iB : std_logic_vector(31 downto 0);		-- output of MUX B
 	signal s_iC : std_logic_vector(31 downto 0);		-- output of MUX C
 	signal s_iD : std_logic_vector(31 downto 0);		-- output of MUX D
+	signal s_iE : std_logic_vector(31 downto 0);		-- output of MUX E (test signal)
 	
 	
 	-- N-bit 2:1 MUX
 	component mux2t1_N
 		port(i_S          : in std_logic;
-			 i_D0         : in std_logic_vector(31 downto 0);
-			 i_D1         : in std_logic_vector(31 downto 0);
-			 o_O          : out std_logic_vector(31 downto 0));
+			 i_D0         : in std_logic_vector(N-1 downto 0);
+			 i_D1         : in std_logic_vector(N-1 downto 0);
+			 o_O          : out std_logic_vector(N-1 downto 0));
 	end component;
 	
 	begin
@@ -48,7 +49,7 @@ architecture structural of barrelshift_32 is
 		g_MUXA: mux2t1_N
 		port MAP(i_S               => i_SHAMT(0),
 				 i_D0			   => i_X,
-				 i_D1              => "0" & i_X(30 downto 0),	-- concatenate 
+				 i_D1              => "0" & i_X(31 downto 1),	-- concatenate 
 				 o_O               => s_iA);
 				 
 		---------------------------------------------------------------------------
@@ -57,7 +58,7 @@ architecture structural of barrelshift_32 is
 		g_MUXB: mux2t1_N
 		port MAP(i_S               => i_SHAMT(1),
 				 i_D0			   => s_iA,
-				 i_D1              => "00" & s_iA(29 downto 0),
+				 i_D1              => "00" & s_iA(31 downto 2),
 				 o_O               => s_iB);
 				 
 		---------------------------------------------------------------------------
@@ -66,7 +67,7 @@ architecture structural of barrelshift_32 is
 		g_MUXC: mux2t1_N
 		port MAP(i_S               => i_SHAMT(2),
 				 i_D0			   => s_iB,
-				 i_D1              => "0000" & s_iB(27 downto 0),
+				 i_D1              => "0000" & s_iB(31 downto 4),
 				 o_O               => s_iC);
 		
 		---------------------------------------------------------------------------
@@ -75,7 +76,7 @@ architecture structural of barrelshift_32 is
 		g_MUXD: mux2t1_N
 		port MAP(i_S               => i_SHAMT(3),
 				 i_D0			   => s_iC,
-				 i_D1              => "00000000" & s_iC(23 downto 0),
+				 i_D1              => "00000000" & s_iC(31 downto 8),
 				 o_O               => s_iD);
   
   		---------------------------------------------------------------------------
@@ -84,6 +85,8 @@ architecture structural of barrelshift_32 is
 		g_MUXE: mux2t1_N
 		port MAP(i_S               => i_SHAMT(4),
 				 i_D0			   => s_iD,
-				 i_D1              => "0000000000000000" & s_iD(15 downto 0),
-				 o_O               => o_Y);			-- final stage, so output to component output
+				 i_D1              => "0000000000000000" & s_iD(31 downto 16),
+				 o_O               => s_iE);		-- final stage, so output to component output
+				 
+		
 end structural;
--- tb_barrelshift_32.vhd
+++ tb_barrelshift_32.vhd
@@ -10,19 +10,64 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity barrelshift_32 is
-  generic(N : integer := 4); -- Generic of type integer for input/output data width. Default value is 32.
+architecture mixed of tb_barrelshift_32 is
+
+	-- Define the total clock period time
+
+	component barrelshift_32 
   
-  port(i_SHAMT      : in std_logic_vector(1 downto 0)		 -- Shift amount input
-       i_D	        : in std_logic;     					 -- Shift direction input (0: left, 1: right)
-       i_T          : in std_logic;     					 -- Shift type input (0: logical, 1: arith.)
-	   i_X          : in std_logic_vector(N-1 downto 0);     -- Data value input
-       o_Y          : out std_logic_vector(N-1 downto 0));   -- Data value output
+		port(i_SHAMT      : in std_logic_vector(4 downto 0);	 -- Shift amount input
+			 i_D	      : in std_logic;     					 -- Shift direction input (0: left, 1: right)
+			 i_T          : in std_logic;     					 -- Shift type input (0: logical, 1: arith.)
+			 i_X          : in std_logic_vector(31 downto 0);   -- Data value input
+			 o_Y          : out std_logic_vector(31 downto 0));  -- Data value output
+			 
+	end component;
+	
+	-- Define test signals
+	signal s_iSHAMT			:  std_logic_vector(4 downto 0);	 -- Shift amount input
+	signal s_iD				:  std_logic;     					 -- Shift direction input (0: left, 1: right)
+	signal s_iT				:  std_logic;     					 -- Shift type input (0: logical, 1: arith.)
+	signal s_iX				:  std_logic_vector(31 downto 0);    -- Data value input
+	signal s_oY				:  std_logic_vector(31 downto 0);  -- Data value output
+	
+	begin
+	
+	DUT0: barrelshift_32
+	port map(
+			 i_SHAMT => s_iSHAMT,
+			 i_D => s_iD,
+			 i_T => s_iT,
+			 i_X => s_iX,
+			 o_Y => s_oY);
+			 
+	
+	P_TEST_CASES: process
+		begin
+			-- Test case 1:
+			-- Shift an input of 2 right by 3
+			wait for 50 ns;
+			
+			s_iSHAMT    <= "00011";  
+			s_iD	    <= '0';
+			s_iT 		<= '0';
+			s_iX	    <= "00000000000000000000000000000010";
+			s_oY 		<= "00000000000000000000000000000000";
+			
+			wait for 50 ns;
+			-- Expected output: s_oY = 0
+			
+			-- Test case 2:
+			-- Shift an input of 0xF0000000 right by 31
+			wait for 50 ns;
+			s_iSHAMT    <= "11111";  
+			s_iD	    <= '0';
+			s_iT 		<= '0';
+			s_iX	    <= "11110000000000000000000000000000";
+			s_oY 		<= "00000000000000000000000000000000";
+			wait for 50 ns;
+			-- Expected output: s_oY = 1
 
-end barrelshift_32;
-
-architecture structural of barrelshift_32 is
-
--- Todo: structural architecture, test process
-
-end structural;
+    
+	end process;
+end mixed;
