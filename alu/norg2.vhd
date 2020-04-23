ff5c833671cff55ebc484c71d9c35ad7047b1c9c
declanc1 <64104105+declanc1@users.noreply.github.com>
2020-04-23 13:30:46 -0500

    Update norg2.vhd

--- alu/norg2.vhd
+++ alu/norg2.vhd
@@ -22,9 +22,9 @@ use IEEE.std_logic_1164.all;
 
 entity norg2 is
 
-  port(i_A          : in std_logic;
-       i_B          : in std_logic;
-       o_F          : out std_logic);
+  port(i_A          : in std_logic_vector(31 downto 0);
+       i_B          : in std_logic_vector(31 downto 0);
+       o_F          : out std_logic_vector(31 downto 0));
 
 end norg2;
 
