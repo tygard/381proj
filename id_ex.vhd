2b275bdfecda7c18f90f7766c545ed933839006b
declanc1 <64104105+declanc1@users.noreply.github.com>
2020-05-01 02:39:50 -0500

    Update id_ex.vhd

--- id_ex.vhd
+++ id_ex.vhd
@@ -3,7 +3,10 @@ use IEEE.std_logic_1164.all;
 
 entity id_ex is
   generic(N : integer := 32); 
-  port(	i_DestReg        : in std_logic_vector(N-1 downto 0); 
+  port(	i_CLK       : in std_logic;
+		i_RST		: in std_logic;
+		i_WE		: in std_logic;
+		i_DestReg        : in std_logic_vector(N-1 downto 0); 
 		i_jump       : in std_logic;  
 		i_branch
 		i_memread
@@ -52,31 +55,10 @@ component nbit_Reg  is
 --------------------------------------------------------------------------------------------
 --architecture structural of f_alu is
 
---signals:
-	signal s_iS : std_logic;							
+--signals:							
 		
 	signal s_multu : std_logic_vector(63 downto 0);
-	signal s_multu_first : std_logic_vector(N-1 downto 0);
-	signal s_multu_last : std_logic_vector(N-1 downto 0);
-	
 	signal s_multu_carry : std_logic;
-	
-	signal s_add : std_logic_vector(N-1 downto 0);	
-	signal s_add_carry : std_logic;	
-	signal s_sub: std_logic_vector(N-1 downto 0);	
-	signal s_sub_carry: std_logic;	
-	signal s_and : std_logic_vector(N-1 downto 0);	
-	
-	signal s_or : std_logic_vector(N-1 downto 0);	
-	signal s_xor : std_logic_vector(N-1 downto 0);	
-	signal s_nor : std_logic_vector(N-1 downto 0);	
-	signal s_slt_bit : std_logic;	
-	signal s_slt : std_logic_vector(N-1 downto 0);	
-
-	signal s_beq : std_logic_vector(N-1 downto 0);	
-	signal s_bne : std_logic_vector(N-1 downto 0);
-	signal s_beq_bit : std_logic;	
-	signal s_bne_bit : std_logic;	
   
 begin
 
@@ -103,12 +85,5 @@ g_or: org32
 		port MAP(i_A   =>  i_A,
 				i_B	   =>  i_B,
 				o_F   =>  s_or);	
-				
---xor-------------------------------- CHANGE TO 32
-g_xor: xorg32
-		port MAP(i_A   =>  i_A,
-				i_B	   =>  i_B,
-				o_F   => s_xor );
 	
-----------------------------
 end structural;
