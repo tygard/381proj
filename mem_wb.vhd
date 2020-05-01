2313e87f8f10a75526579a810c7f2ccd331f47f9
declanc1 <64104105+declanc1@users.noreply.github.com>
2020-05-01 04:19:20 -0500

    Update mem_wb.vhd

--- mem_wb.vhd
+++ mem_wb.vhd
@@ -8,21 +8,27 @@ entity mem_wb is
 		i_WE		: in std_logic;
 		
 		
-		i_memtoreg       : in std_logic_vector(N-1 downto 0); 
-		i_mux1      : in std_logic_vector(N-1 downto 0); 
+		i_branch     : in std_logic_vector(N-1 downto 0); 
+		i_memtoreg      : in std_logic_vector(N-1 downto 0); 
+		i_memWrite    : in std_logic_vector(N-1 downto 0);
+		i_ALU_overflow    : in std_logic;
+		i_ALU_result    : in std_logic_vector(N-1 downto 0);
+		i_data_1   : in std_logic_vector(N-1 downto 0);
+		i_mux4  : in std_logic_vector(N-1 downto 0);
 		
-		
-		
-		o_memtoreg	    : out std_logic_vector (N-1 downto 0); 
-		o_mux1      : out std_logic_vector(N-1 downto 0)); 
+		o_branch     : in std_logic_vector(N-1 downto 0); 
+		o_memtoreg      : in std_logic_vector(N-1 downto 0); 
+		o_memWrite    : in std_logic_vector(N-1 downto 0);
+		o_ALU_overflow    : in std_logic;
+		o_ALU_result    : in std_logic_vector(N-1 downto 0);
+		o_data_1   : in std_logic_vector(N-1 downto 0);
+		o_mux4  : in std_logic_vector(N-1 downto 0);
 	
 end mem_wb;
 
 architecture structural of mem_wb is
 
 --components:
-  
---nbit_Reg
 component nbit_Reg  is
 	port(
 		i_CLK       : in std_logic;
@@ -31,37 +37,64 @@ component nbit_Reg  is
 		i_D        : in std_logic_vector(N-1 downto 0);
 		o_Q         : out std_logic_vector(N-1 downto 0));
   end component;
-
 --------------------------------------------------------------------------------------------
 --architecture structural of f_alu is
-
---signals:							
-		
-	signal s_multu : std_logic_vector(63 downto 0);
-	signal s_multu_carry : std_logic;
-  
 begin
 
---arch:
+--reg-------------------------------
+g_nbitReg1: nbit_Reg
+		port MAP(i_CLK  =>  i_CLK,
+				i_RST	=>  i_RST,
+				i_WE    =>    i_WE,
+				i_D     => i_branch
+				o_Q	    =>  o_branch);
+				
+--reg-------------------------------
+g_nbitReg2: nbit_Reg
+		port MAP(i_CLK  =>  i_CLK,
+				i_RST	=>  i_RST,
+				i_WE    =>    i_WE,
+				i_D     => i_memtoreg
+				o_Q	    =>  o_memtoreg);
+				
+--reg-------------------------------
+g_nbitReg3: nbit_Reg
+		port MAP(i_CLK  =>  i_CLK,
+				i_RST	=>  i_RST,
+				i_WE    =>    i_WE,
+				i_D     => i_memWrite
+				o_Q	    =>  o_memWrite);
+				
+--reg-------------------------------
+g_nbitReg4: nbit_Reg
+		port MAP(i_CLK  =>  i_CLK,
+				i_RST	=>  i_RST,
+				i_WE    =>    i_WE,
+				i_D     => i_ALU_overflow
+				o_Q	    =>  o_ALU_overflow);
+				
+--reg-------------------------------
+g_nbitReg5: nbit_Reg
+		port MAP(i_CLK  =>  i_CLK,
+				i_RST	=>  i_RST,
+				i_WE    =>    i_WE,
+				i_D     => i_ALU_result
+				o_Q	    =>  o_ALU_result);
+				
+--reg-------------------------------
+g_nbitReg6: nbit_Reg
+		port MAP(i_CLK  =>  i_CLK,
+				i_RST	=>  i_RST,
+				i_WE    =>    i_WE,
+				i_D     => i_data_1
+				o_Q	    =>  o_data_1);
+				
+--reg-------------------------------
+g_nbitReg7: nbit_Reg
+		port MAP(i_CLK  =>  i_CLK,
+				i_RST	=>  i_RST,
+				i_WE    =>    i_WE,
+				i_D     => i_mux4
+				o_Q	    =>  o_mux4);
 
---mult-------------------------------
-g_mult: m_N_bit
-		port MAP(i_A     =>  i_A,
-				i_B	     =>  i_B,
-				o_S      =>    s_multu,
-				o_Cout	 =>  s_multu_carry);--make 1 bit
-				
---or--------------------------------- CHANGE TO 32
-g_or: org32
-		port MAP(i_A   =>  i_A,
-				i_B	   =>  i_B,
-				o_F   =>  s_or);	
-				
---xor-------------------------------- CHANGE TO 32
-g_xor: xorg32
-		port MAP(i_A   =>  i_A,
-				i_B	   =>  i_B,
-				o_F   => s_xor );
-	
-----------------------------
 end structural;
