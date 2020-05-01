9a4ae9cd5ef1cb907e6ff6627fbf47b0be1e0bc3
tygard <tygard@iastate.edu>
2020-05-01 06:06:48 -0500

    updates

--- invg32.vhd
+++ invg32.vhd
@@ -1,12 +1,12 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity invg is
+entity invg32 is
 
   port(i_A          : in std_logic_vector(31 downto 0);
        o_F          : out std_logic_vector(31 downto 0));
 
-end invg;
+end invg32;
 
 architecture dataflow of invg is
 begin
