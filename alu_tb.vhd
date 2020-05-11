2125e970a542d836f02f4fed0204a6c7ca04c411
declanc1 <64104105+declanc1@users.noreply.github.com>
2020-05-11 14:01:41 -0500

    Update alu_tb.vhd

--- alu_tb.vhd
+++ alu_tb.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_textio.all;  -- For logic types I/O
@@ -36,10 +35,14 @@ begin
 
   DUT0: f_alu
   generic map(N => N)
-  port map( i_S      => s_i_S,
-            i_D0     => s_i_D0,
-            i_D1     => s_i_D1,
-            o_O      => s_o_O);
+  port map(
+  i_A => s_i_A,
+  i_B => s_i_B,
+  i_C => s_i_c);
+  -- port map( i_S      => s_i_S,
+         -- -- i_D0     => s_i_D0,
+         -- -- i_D1     => s_i_D1,
+         -- -- o_O      => s_o_O);
   
   
   P_TEST_CASES: process
@@ -49,168 +52,168 @@ begin
   
 s_i_A  <= X"00000004";
 s_i_B  <= X"00000000";
-s_i_C  <= "01001" --addu
+s_i_C  <= "01001"; --addu
  --4+0 should be 4
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000008";
 s_i_B  <=  X"00000000";
-s_i_C  <= "01010" --sub
+s_i_C  <= "01010"; --sub
 --8-0 should be 8
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000008";
 s_i_B  <=  X"00000002";
-s_i_C  <= "01000" --add
+s_i_C  <= "01000"; --add
 --8+2 should be 10
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000008";
 s_i_B  <=  X"00000004";
-s_i_C  <= "01011" --subu
+s_i_C  <= "01011"; --subu
 --8-4 = 4
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000FFF";
 s_i_B  <=  X"00000F00";
-s_i_C  <= "01100" --and
+s_i_C  <= "01100"; --and
 --answer should be 00000F00
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000000";
 s_i_B  <=  X"0000FFF0";
-s_i_C  <= "01101" --or
+s_i_C  <= "01101"; --or
 --answer is 0000FFF0
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000F0F";
 s_i_B  <=  X"000000F0";
-s_i_C  <= "01110" --xor
+s_i_C  <= "01110"; --xor
 --answer should be 00000FFF
 
 wait for 100 ns;
   
 s_i_A  <=  X"0000000A";
 s_i_B  <=  X"0000000B";
-s_i_C  <= "01111" --nor
+s_i_C  <= "01111"; --nor
 --answer should be FFFFFFF4?
 
 wait for 100 ns;
   
 s_i_A  <=  X"0000000F";
 s_i_B  <=  X"0000000E";
-s_i_C  <= "10000" --slt
+s_i_C  <= "10000"; --slt
 --answer should be 1 because A is greater than B.
 
 wait for 100 ns;
   
 s_i_A  <=  X"0000000F";
 s_i_B  <=  X"00000FF0";
-s_i_C  <= "10001" --sltu
+s_i_C  <= "10001"; --sltu
 --answer should be 0 because B is greater
 
 wait for 100 ns;
   
 s_i_A  <=  X"0000000A";
 s_i_B  <=  X"00000001";
-s_i_C  <= "10110" --addi
+s_i_C  <= "10110"; --addi
 --answer should be B
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000004";
 s_i_B  <=  X"00000005";
-s_i_C  <= "10111" --addiu
+s_i_C  <= "10111"; --addiu
 --answer should be 9
 
 wait for 100 ns;
   
 s_i_A  <=  X"0000000F";
 s_i_B  <=  X"00000001";
-s_i_C  <= "11000" --slti
+s_i_C  <= "11000"; --slti
 --answer should be 1 because A is greater than B
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000000";
 s_i_B  <=  X"0000000A";
-s_i_C  <= "11001" --sltiu
+s_i_C  <= "11001"; --sltiu
 --answer should be 0 because B is greater than A
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000101";
 s_i_B  <=  X"00000100";
-s_i_C  <= "11010" --andi
+s_i_C  <= "11010"; --andi
 --answer should be 00000100
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000000";
 s_i_B  <=  X"000000FF";
-s_i_C  <= "11011" --ori
+s_i_C  <= "11011"; --ori
 --answer should be 000000FF
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000F0F";
 s_i_B  <=  X"000000F0";
-s_i_C  <= "11100" --xori
+s_i_C  <= "11100"; --xori
 --answer should be 00000FFF
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000001";
 s_i_B  <=  X"00000002";
-s_i_C  <= "00000" --sll
+s_i_C  <= "00000"; --sll
 --answer should be 00000004
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000004";
 s_i_B  <=  X"00000001";
-s_i_C  <= "00001" --srl
+s_i_C  <= "00001"; --srl
 --answer should be 00000002
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000004";
 s_i_B  <=  X"00000001";
-s_i_C  <= "00010" --sra
+s_i_C  <= "00010"; --sra
 --answer should be 00000002
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000004";
 s_i_B  <=  X"00000002";
-s_i_C  <= "00011" --sllv
+s_i_C  <= "00011"; --sllv
 --answer should be 00000010
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000004";
 s_i_B  <=  X"00000002";
-s_i_C  <= "00100" --xor
+s_i_C  <= "00100"; --xor
 --answer should be 00000001
 
 wait for 100 ns;
   
 s_i_A  <=  X"0000000C";
 s_i_B  <=  X"00000002";
-s_i_C  <= "00101" --srav
+s_i_C  <= "00101"; --srav
 --answer should be 00000003
 
 wait for 100 ns;
   
 s_i_A  <=  X"00000002";
 s_i_B  <=  X"00000002";
-s_i_C  <= "00111" --multu
+s_i_C  <= "00111"; --multu
 --answer should be 00000004
 
 
